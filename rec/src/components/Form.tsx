23ff241885cf881615c8805b0c4f5042d343a9c9
Kimthanh11 <18047@student.vgu.edu.vn>
2022-05-05 14:51:22 +0700

    Update recruit form

--- rec/src/components/Form.tsx
+++ rec/src/components/Form.tsx
@@ -1,37 +1,41 @@
 import React from 'react';
 function Form() {
   return (
-    <div className='form'>
+    <div className='pform'>
         <h1>RECRUIT FORM</h1>
         <h4>Amet minim mollit non deserunt ullamco est sit aliquatgo dolor do amet sint. Velit officia consequat Amet minimuu mollit non deserunt ullamco.</h4>
-        <form>
-            <div>
-                <label>Name:</label>
-                <input type="text"/>
-                <label>Student ID:</label>
-                <input type="text"/>
+        <form className='form'>
+            <div className="formgrid">
+                <div className='half'>
+                    <label>Name:</label>
+                    <input type="text"/>
+                    <label>Student ID:</label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>Facebook link:</label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>Phone number:</label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>Short introduction about yourself:</label>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <label>Short introduction about your project:</label>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <label>Number of members required:</label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <button>Submit</button>
+                </div>
             </div>
-            <div>
-                <label>Faceebook link:</label>
-                <input type="text"/>
-            </div>
-            <div>
-                <label>Phone number:</label>
-                <input type="text"/>
-            </div>
-            <div>
-                <label>Short introduction about yourself:</label>
-                <textarea></textarea>
-            </div>
-            <div>
-                <label>Short introduction about your project:</label>
-                <textarea></textarea>
-            </div>
-            <div>
-                <label>Number of members required:</label>
-                <input type="text"/>
-            </div>
-            <input type="submit" value="Submit"/>
         </form>
     </div>
   );
