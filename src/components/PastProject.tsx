9f182b64a657797cad13c66961017f0d9704dcdb
Kimthanh11 <18047@student.vgu.edu.vn>
2022-05-06 11:55:53 +0700

    Update HTML

--- src/components/PastProject.tsx
+++ src/components/PastProject.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 function PastProject() {
   return (
     <div>
+      <h1>PAST PROJECT</h1>
+        <hr></hr>
+        <div>
+          <h4>Topic</h4>
+          <div>Description</div>
+          <button>See more</button>
 
+          <h4>Topic</h4>
+          <div>Description</div>
+          <button>See more</button>
+        </div>
     </div>
   );
 }
--- src/components/RecruitProject.tsx
+++ src/components/RecruitProject.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 function RecruitProject() {
   return (
     <div>
+      <h1>RECRUIT PROJECT</h1>
+        <hr></hr>
+        <div>
+          <h4>Topic</h4>
+          <div>Description</div>
+          <button>See more</button>
 
+          <h4>Topic</h4>
+          <div>Description</div>
+          <button>See more</button>
+        </div>
     </div>
   );
 }
