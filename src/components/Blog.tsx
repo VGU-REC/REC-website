0d7f85cc8efcdfcaf2b9947f89578c829e43c4fb
Kimthanh11 <18047@student.vgu.edu.vn>
2022-05-09 21:38:30 +0700

    Update image

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -4,6 +4,13 @@ function Blog() {
     <div>
       <div>Carousel</div>
       <div>
+      <img
+            src="test.jpg"
+            alt="Consulting"
+          />
+
+      </div>
+      <div>
           <div>Image here</div>
           <h4>Headline</h4>
           <div>Description</div>
