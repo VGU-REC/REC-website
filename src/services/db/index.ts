2f1a6fe0c28f8a9f6399557138a64ee453a90bcf
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-05-09 18:35:45 +0700

    Change type of id param to string of db functions

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -1,26 +1,23 @@
-import { DbCollection, DbDoc, DbPrimaryKey, OmitPrimaryKey } from "interfaces";
+import { DbCollection, DbDoc, OmitPrimaryKey } from "interfaces";
 
 declare function set<T extends DbCollection>(
   col: T,
-  id: DbPrimaryKey<T>,
+  id: string,
   data: OmitPrimaryKey<DbDoc<T>>
 ): Promise<void>;
 
 declare function get<T extends DbCollection>(col: T): Promise<DbDoc<T>[]>;
 declare function get<T extends DbCollection>(
   col: T,
-  id: DbPrimaryKey<T>
+  id: string
 ): Promise<DbDoc<T>>;
 
 declare function update<T extends DbCollection>(
   col: T,
-  id: DbPrimaryKey<T>,
+  id: string,
   data: Partial<OmitPrimaryKey<DbDoc<T>>>
 ): Promise<void>;
 
-declare function del<T extends DbCollection>(
-  col: T,
-  id: DbPrimaryKey<T>
-): Promise<void>;
+declare function del<T extends DbCollection>(col: T, id: string): Promise<void>;
 
 export { set, get, update, del };
