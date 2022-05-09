7295dc8eefdcd606a35a7e113aca10736e348a67
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-05-09 18:13:14 +0700

    Add fn decl for db api

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -1,1 +1,26 @@
-export {};
+import { DbCollection, DbDoc, DbPrimaryKey, OmitPrimaryKey } from "interfaces";
+
+declare function set<T extends DbCollection>(
+  col: T,
+  id: DbPrimaryKey<T>,
+  data: OmitPrimaryKey<DbDoc<T>>
+): Promise<void>;
+
+declare function get<T extends DbCollection>(col: T): Promise<DbDoc<T>[]>;
+declare function get<T extends DbCollection>(
+  col: T,
+  id: DbPrimaryKey<T>
+): Promise<DbDoc<T>>;
+
+declare function update<T extends DbCollection>(
+  col: T,
+  id: DbPrimaryKey<T>,
+  data: Partial<OmitPrimaryKey<DbDoc<T>>>
+): Promise<void>;
+
+declare function del<T extends DbCollection>(
+  col: T,
+  id: DbPrimaryKey<T>
+): Promise<void>;
+
+export { set, get, update, del };
