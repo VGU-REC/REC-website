13f386c8163300d689b3c799bc8067c7e8238877
Kimthanh11 <18047@student.vgu.edu.vn>
2022-06-04 12:35:49 +0700

    Update footer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ function Navbar() {
             <div>
               <div className="flex justify-end mt-6">
                 <div className="flex justify-center">
-                  <div className="mb-2 xl:w-96">
+                  <div className="xl:w-96">
                     <div className="input-group relative flex flex-wrap items-stretch w-full mb-4 rounded">
                       <input type="search" className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" />
                       <span className="input-group-text flex items-center px-3 py-1.5 text-base font-normal text-gray-700 text-center whitespace-nowrap rounded" id="basic-addon2">
