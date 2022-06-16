cb2c7122f65a2c7057403273124b2347767223f0
Kimthanh11 <18047@student.vgu.edu.vn>
2022-06-16 17:08:21 +0700

    Update carousel

--- client/src/components/CarouselMultipleItems.tsx
+++ client/src/components/CarouselMultipleItems.tsx
@@ -1,8 +1,10 @@
 import { CardNoImage } from "./CardNoImage";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Children, FC } from "react";
+import { JsxAttributeLike } from "typescript";
 
-interface content {
+interface Props {
   show: number;
+  children: { headline: string; description: string }[];
 }
 
 // type Props = {
@@ -10,66 +12,53 @@ interface content {
 //   children: JSX.Element[];
 // }
 
-function CarouselMultipleItems({show}: content) {
-  const [currentIndex, setCurrentIndex] = useState(0)
-  // const [length, setLength] = useState(children.length)
-  // useEffect(() => {
-  //   setLength(children.length)
-  // }, [children])
-  const length = 16;
+const CarouselMultipleItems: FC<Props> = ({ show, children }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [length, setLength] = useState(children.length);
+  useEffect(() => {
+    setLength(children.length);
+  }, [children]);
+  //const length = 5;
   const next = () => {
-    if (currentIndex < (length - show)) {
-      setCurrentIndex(prevState => prevState + 1)
+    if (currentIndex < length - show) {
+      setCurrentIndex((prevState) => prevState + 1);
     }
-  }
+  };
 
   const prev = () => {
     if (currentIndex > 0) {
-      setCurrentIndex(prevState => prevState - 1)
+      setCurrentIndex((prevState) => prevState - 1);
     }
-  }
+  };
 
   return (
     <div>
       <div className="carousel-wrapper">
-          {
-              currentIndex > 0 &&
-              <button onClick={prev} className="left-arrow">
-                  &lt;
-              </button>
-          }
+        {currentIndex > 0 && (
+          <button onClick={prev} className="left-arrow">
+            &lt;
+          </button>
+        )}
         <div className="carousel-content-wrapper">
           <div
-              className={`carousel-content show-${show}`}
-              style={{ transform: `translateX(-${currentIndex * (100 / show)}%)` }}
-          >   
-               <CardNoImage headline='Project 1' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 2' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 3' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 4' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 5' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 6' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 7' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 8' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 9' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 10' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 11' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 12' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 13' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 14' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 15' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-              <CardNoImage headline='Project 16' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>   
+            className={`carousel-content show-${show}`}
+            style={{
+              transform: `translateX(-${currentIndex * (100 / show)}%)`,
+            }}
+          >
+            {children.map(({ headline, description }) => (
+              <CardNoImage headline={headline} description={description} />
+            ))}
           </div>
         </div>
-          {
-              currentIndex < (length - show) &&
-              <button onClick={next} className="right-arrow">
-                  &gt;
-              </button>
-          }
+        {currentIndex < length - show && (
+          <button onClick={next} className="right-arrow">
+            &gt;
+          </button>
+        )}
       </div>
     </div>
   );
-}
+};
 
-export {CarouselMultipleItems}
+export { CarouselMultipleItems };
--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,69 +1,157 @@
-import { Card, Carousel, CardNoImage, CarouselMultipleItems} from 'components';
-import {Link} from "react-router-dom";
+import { Card, Carousel, CardNoImage, CarouselMultipleItems } from "components";
+import { Link } from "react-router-dom";
 
 function Projects() {
   return (
-    <div className='bg-gray-200'>
-      <div className='bg-white grid grid-cols-2 h-96'>
-        <div className='col-1 pt-20'>
-          <h1 className='text-xl p-3 font-bold text-center'>PAST PROJECT</h1>
-          <div className='flex justify-center'>
-            <div className='w-96'>
-              <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequatAmet minim</p>
+    <div className="bg-gray-200">
+      <div className="bg-white grid grid-cols-2 h-96">
+        <div className="col-1 pt-20">
+          <h1 className="text-xl p-3 font-bold text-center">PAST PROJECT</h1>
+          <div className="flex justify-center">
+            <div className="w-96">
+              <p>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequatAmet minim
+              </p>
             </div>
           </div>
-          <div className='flex justify-center pt-3'>
+          <div className="flex justify-center pt-3">
             <Link to="/project/past">
-              <button type="button" className="inline-block px-4 py-2.5 bg-orange-400 text-white font-bold text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">See more</button>
+              <button
+                type="button"
+                className="inline-block px-4 py-2.5 bg-orange-400 text-white font-bold text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                See more
+              </button>
             </Link>
           </div>
         </div>
-        <div className='col-2 pt-6'>
+        <div className="col-2 pt-6">
           <Carousel />
         </div>
       </div>
 
       <div className="grid grid-cols-1 divide-black divide-y-4">
         <div>
-          <h1 className='text-xl p-3 font-bold'>PENDING PROJECT</h1>
+          <h1 className="text-xl p-3 font-bold">PENDING PROJECT</h1>
         </div>
         <div>
-          <CarouselMultipleItems show={3}/>
-          <Link to="/project/recruiting"><h1 className='text-xl p-3 font-bold hover:text-orange-600'>RECRUIT PROJECT</h1></Link>
+          <CarouselMultipleItems show={3}>
+            {[
+              {
+                headline: "Project 1",
+                description:
+                  "Some quick example text to build on the card title and make up the bulk of the card's content.",
+              },
+              {
+                headline: "Project 2",
+                description:
+                  "Some quick example text to build on the card title and make up the bulk of the card's content.",
+              },
+              {
+                headline: "Project 3",
+                description:
+                  "Some quick example text to build on the card title and make up the bulk of the card's content.",
+              },
+              {
+                headline: "Project 4",
+                description:
+                  "Some quick example text to build on the card title and make up the bulk of the card's content.",
+              },
+              {
+                headline: "Project 5",
+                description:
+                  "Some quick example text to build on the card title and make up the bulk of the card's content.",
+              },
+            ]}
+          </CarouselMultipleItems>
+          <Link to="/project/recruiting">
+            <h1 className="text-xl p-3 font-bold hover:text-orange-600">
+              RECRUIT PROJECT
+            </h1>
+          </Link>
         </div>
         <div>
-          <div className='grid grid-cols-3 p-4'>
-            <div className='cols-1'>
-              <Card image='img1.jpg' alt='Image' headline='Headline' description='This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'/>
-              <Card image='img2.jpg' alt='Image' headline='Headline' description='This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'/>
+          <div className="grid grid-cols-3 p-4">
+            <div className="cols-1">
+              <Card
+                image="img1.jpg"
+                alt="Image"
+                headline="Headline"
+                description="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+              />
+              <Card
+                image="img2.jpg"
+                alt="Image"
+                headline="Headline"
+                description="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+              />
             </div>
-            <div className='cols-2'>
-              <Card image='img3.jpg' alt='Image' headline='Headline' description='This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'/>
-              <Card image='img4.jpg' alt='Image' headline='Headline' description='This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'/>
+            <div className="cols-2">
+              <Card
+                image="img3.jpg"
+                alt="Image"
+                headline="Headline"
+                description="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+              />
+              <Card
+                image="img4.jpg"
+                alt="Image"
+                headline="Headline"
+                description="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+              />
             </div>
-            <div className='cols-3'>
-              <Card image='img5.jpg' alt='Image' headline='Headline' description='This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'/>
-              <Card image='img6.jpg' alt='Image' headline='Headline' description='This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'/>
+            <div className="cols-3">
+              <Card
+                image="img5.jpg"
+                alt="Image"
+                headline="Headline"
+                description="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+              />
+              <Card
+                image="img6.jpg"
+                alt="Image"
+                headline="Headline"
+                description="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+              />
             </div>
           </div>
-          <div className='flex flex-row justify-between'>
-            <Link to="/project/idea"><h1 className='text-xl p-3 font-bold hover:text-orange-600'>IDEA</h1></Link>
-            <div className='pr-5 pb-2'>
+          <div className="flex flex-row justify-between">
+            <Link to="/project/idea">
+              <h1 className="text-xl p-3 font-bold hover:text-orange-600">
+                IDEA
+              </h1>
+            </Link>
+            <div className="pr-5 pb-2">
               <Link to="/project/new">
-                <button type="button" className="inline-block px-8 py-4 bg-orange-400 text-white font-bold text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-600 active:shadow-lg transition duration-150 ease-in-out">New Project</button>
+                <button
+                  type="button"
+                  className="inline-block px-8 py-4 bg-orange-400 text-white font-bold text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-600 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  New Project
+                </button>
               </Link>
             </div>
           </div>
         </div>
-        <div className='grid grid-cols-1 gap-4 content-center p-4'>
+        <div className="grid grid-cols-1 gap-4 content-center p-4">
           <div>
-            <CardNoImage headline='Headline' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+            <CardNoImage
+              headline="Headline"
+              description="Some quick example text to build on the card title and make up the bulk of the card's content."
+            />
           </div>
           <div>
-            <CardNoImage headline='Headline' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+            <CardNoImage
+              headline="Headline"
+              description="Some quick example text to build on the card title and make up the bulk of the card's content."
+            />
           </div>
           <div>
-            <CardNoImage headline='Headline' description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+            <CardNoImage
+              headline="Headline"
+              description="Some quick example text to build on the card title and make up the bulk of the card's content."
+            />
           </div>
         </div>
       </div>
