f000bc2fe5a4b18f5a84dc4570fe5f604e77a955
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-06-26 10:26:31 +0700

    add folder structure

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,42 @@
 import express from "express";
 
+interface Quan {
+  name: string;
+  age: number;
+  married: boolean;
+}
+let userList: Quan[] = [
+  {
+    name: "cc",
+    age: 19,
+    married: false,
+  },
+  {
+    name: "cc1",
+    age: 19,
+    married: true,
+  },
+  {
+    name: "cc2",
+    age: 19,
+    married: false,
+  },
+];
+const tho: string = "hello";
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello tho gae");
+app.use(express.json());
+app.get(/user\/.*thanh$/, (req, res) => {
+  res.json(userList);
 });
-
+app.post("/users/:quanla", (req, res) => {
+  const newUser = req.body;
+  console.log(req.params.quanla);
+  userList.push(newUser);
+  res.send(userList);
+  // console.log(res);
+  // res.sendStatus(200);
+});
 app.listen(5000, () => {
-  console.log("woooooooooo");
   console.log("http://localhost:5000");
+  console.log("woooooooooo");
 });
