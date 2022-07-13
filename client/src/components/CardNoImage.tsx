8b85499fd1f321ec8ef3afc2e58c796654f89c04
Kimthanh11 <18047@student.vgu.edu.vn>
2022-07-13 23:23:43 +0700

    Update carousel slider in Achievements

--- client/src/components/CardNoImage.tsx
+++ client/src/components/CardNoImage.tsx
@@ -9,11 +9,11 @@ const CardNoImage: FC<content> = ({ headline, description }) => {
   return (
     <div>
       <div className="flex justify-center ">
-        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
-          <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
+        <div className="block max-w-sm p-6 bg-white rounded-lg shadow-lg">
+          <h5 className="mb-2 text-xl font-medium leading-tight text-gray-900">
             {headline}
           </h5>
-          <p className="text-gray-700 text-base mb-2">{description}</p>
+          <p className="mb-2 text-base text-gray-700">{description}</p>
           <button
             type="button"
             className="inline-block px-6 py-2.5 bg-orange-400 text-white font-bold text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-600 active:shadow-lg transition duration-150 ease-in-out"
--- client/src/pages/Achievements.tsx
+++ client/src/pages/Achievements.tsx
@@ -1,14 +1,15 @@
+import { CarouselSlider } from "components";
 import { FC } from "react";
 
 const Achievements: FC = () => {
   return (
     <div>
       <div className="relative overflow-hidden bg-no-repeat bg-cover header">
-        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed header2">
-          <div className="flex justify-center items-center h-full">
-            <div className="text-center text-white px-6 md:px-12">
-              <h1 className="text-5xl font-bold mt-0 mb-6">ACHIEVEMENTS</h1>
-              <h3 className="text-3xl font-bold mb-8">
+        <div className="absolute top-0 bottom-0 left-0 right-0 w-full h-full overflow-hidden bg-fixed header2">
+          <div className="flex items-center justify-center h-full">
+            <div className="px-6 text-center text-white md:px-12">
+              <h1 className="mt-0 mb-6 text-5xl font-bold">ACHIEVEMENTS</h1>
+              <h3 className="mb-8 text-3xl font-bold">
                 Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                 amet sint. Velit officia consequatAmet minim
               </h3>
@@ -16,6 +17,45 @@ const Achievements: FC = () => {
           </div>
         </div>
       </div>
+      <CarouselSlider show={3}>
+        {[
+          {
+            id: 0,
+            headline: "Achivement 1",
+            description:
+              "Some quick example text to build on the card title and make up the bulk of the card's content.",
+            src: "img1.jpg",
+          },
+          {
+            id: 1,
+            headline: "Achivement 2",
+            description:
+              "Some quick example text to build on the card title and make up the bulk of the card's content.",
+            src: "img2.jpg",
+          },
+          {
+            id: 2,
+            headline: "Achivement 3",
+            description:
+              "Some quick example text to build on the card title and make up the bulk of the card's content.",
+            src: "img3.jpg",
+          },
+          {
+            id: 3,
+            headline: "Achivement 4",
+            description:
+              "Some quick example text to build on the card title and make up the bulk of the card's content.",
+            src: "img4.jpg",
+          },
+          {
+            id: 4,
+            headline: "Achivement 5",
+            description:
+              "Some quick example text to build on the card title and make up the bulk of the card's content.",
+            src: "img5.jpg",
+          },
+        ]}
+      </CarouselSlider>
     </div>
   );
 };
