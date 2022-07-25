5ac3efd7406667ee7055c50d3fd78571e63a3e08
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-07-25 18:03:23 +0700

    Add Tooltip component

--- client/src/components/RichTextContent/Toolbar.tsx
+++ client/src/components/RichTextContent/Toolbar.tsx
@@ -5,6 +5,7 @@ import {
   MarkButton,
   ColorPicker,
 } from "./text-format";
+import { Tooltip } from "./Tooltip";
 
 const Toolbar: FC = () => {
   return (
@@ -12,12 +13,24 @@ const Toolbar: FC = () => {
       <FontSelect />
       <FontSizeInput />
 
-      <MarkButton format="bold" icon="format_bold" />
-      <MarkButton format="italic" icon="format_italic" />
-      <MarkButton format="underline" icon="format_underlined" />
-      <MarkButton format="strike" icon="strikethrough_s" />
-      <MarkButton format="sub" icon="subscript" />
-      <MarkButton format="super" icon="superscript" />
+      <Tooltip display="Bold (Ctrl+B)">
+        <MarkButton format="bold" icon="format_bold" />
+      </Tooltip>
+      <Tooltip display="Italic (Ctrl+I)">
+        <MarkButton format="italic" icon="format_italic" />
+      </Tooltip>
+      <Tooltip display="Underline (Ctrl+U)">
+        <MarkButton format="underline" icon="format_underlined" />
+      </Tooltip>
+      <Tooltip display="Strikethrough (Alt+Shift+5)">
+        <MarkButton format="strike" icon="strikethrough_s" />
+      </Tooltip>
+      <Tooltip display="Subscript (Ctrl+,)">
+        <MarkButton format="sub" icon="subscript" />
+      </Tooltip>
+      <Tooltip display="Superscript (Ctrl+.)">
+        <MarkButton format="super" icon="superscript" />
+      </Tooltip>
 
       <ColorPicker format="color" icon="format_color_text" />
       <ColorPicker format="bgColor" icon="format_color_fill" />
