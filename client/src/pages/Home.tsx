a22d4b63093de4c92c9ab0dfe79a8e4b44352ee4
Kimthanh11 <18047@student.vgu.edu.vn>
2022-08-03 16:08:02 +0700

    Update responsive home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,13 +5,33 @@ const Home: FC = () => {
   return (
     <div>
       <Carousel />
-      <div className="grid grid-cols-1 divide-black divide-y-4">
+      <div className="grid grid-cols-1 divide-y-4 divide-black">
         <div>
-          <h1 className="text-xl p-3 font-bold">ACTIVITY</h1>
+          <h1 className="p-3 text-xl font-bold">ACTIVITY</h1>
         </div>
         <div>
-          <div className="grid grid-cols-2">
-            <div className="cols-1">
+          <div className="md:grid md:grid-cols-2">
+            <div className="md:cols-2">
+              <div className="pt-3">
+                <CardNoImage
+                  headline="Headline"
+                  description="Some quick example text to build on the card title and make up the bulk of the card's content."
+                />
+              </div>
+              <div className="pt-3">
+                <CardNoImage
+                  headline="Headline"
+                  description="Some quick example text to build on the card title and make up the bulk of the card's content."
+                />
+              </div>
+              <div className="pt-3">
+                <CardNoImage
+                  headline="Headline"
+                  description="Some quick example text to build on the card title and make up the bulk of the card's content."
+                />
+              </div>
+            </div>
+            <div className="md:cols-1">
               <Card
                 image="img1.jpg"
                 alt="Image"
@@ -31,33 +51,13 @@ const Home: FC = () => {
                 description="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
               />
             </div>
-            <div className="cols-2">
-              <div className="pt-3">
-                <CardNoImage
-                  headline="Headline"
-                  description="Some quick example text to build on the card title and make up the bulk of the card's content."
-                />
-              </div>
-              <div className="pt-3">
-                <CardNoImage
-                  headline="Headline"
-                  description="Some quick example text to build on the card title and make up the bulk of the card's content."
-                />
-              </div>
-              <div className="pt-3">
-                <CardNoImage
-                  headline="Headline"
-                  description="Some quick example text to build on the card title and make up the bulk of the card's content."
-                />
-              </div>
-            </div>
           </div>
 
-          <h1 className="text-xl p-3 font-bold">PROJECT</h1>
+          <h1 className="p-3 text-xl font-bold">PROJECT</h1>
         </div>
         <div>
-          <div className="grid grid-cols-2">
-            <div className="cols-1">
+          <div className="md:grid md:grid-cols-2">
+            <div className="md:cols-1">
               <Card
                 image="img4.jpg"
                 alt="Image"
@@ -71,7 +71,7 @@ const Home: FC = () => {
                 description="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
               />
             </div>
-            <div className="cols-2">
+            <div className="md:cols-2">
               <Card
                 image="img6.jpg"
                 alt="Image"
@@ -86,25 +86,25 @@ const Home: FC = () => {
               />
             </div>
           </div>
-          <h1 className="text-xl p-3 font-bold">BLOG</h1>
+          <h1 className="p-3 text-xl font-bold">BLOG</h1>
         </div>
         <div>
-          <div className="grid grid-cols-2 pb-20">
-            <div className="h-96 flex justify-center mt-3">
+          <div className="pb-20 navbar:grid navbar:grid-cols-2">
+            <div className="flex justify-center mt-3 mb-24 h-96 navbar:cols-1">
               <div className="pl-3">
                 <img className="w-full h-96" src="img8.jpg" alt="" />
-                <div className="p-6 flex flex-col justify-start bg-white">
-                  <h5 className="text-gray-900 text-xl font-medium">
+                <div className="flex flex-col p-6 text-center bg-white navbar:justify-start">
+                  <h5 className="text-xl font-medium text-gray-900">
                     Headline
                   </h5>
-                  <p className="text-gray-700 text-base mb-4">
+                  <p className="mb-4 text-base text-gray-700">
                     Some quick example text to build on the card title and make
                     up the bulk of the card'scontent.
                   </p>
                 </div>
               </div>
             </div>
-            <div className="mt-3">
+            <div className="mt-3 navbar:grid-cols-2">
               <Card
                 image="img9.jpg"
                 alt="Image"
