67cbc509c722a014297822b4f829d58ef818737d
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-08-19 12:07:10 +0700

    change to CustomizeRouter to Partial Type

--- server/src/types/Router.d.ts
+++ server/src/types/Router.d.ts
@@ -1,9 +1,5 @@
 export interface CustomizeRouter {
-  generateIdFrom?: string;
-  limit?: number;
-  orderBy?: string;
+  generateIdFrom: string;
+  limit: number;
+  orderBy: string;
 }
-type Concreate<T> = {
-  [properties in keyof T]-?: T[properties];
-};
-export type ConcreateRouter = Concreate<CustomizeRouter>;
