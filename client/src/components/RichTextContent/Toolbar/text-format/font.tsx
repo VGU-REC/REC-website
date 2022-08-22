601de01e3c56f01c68023dae7f3352a0f996340f
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-08-22 19:04:51 +0700

    Update FontSizeInput

--- client/src/components/RichTextContent/Toolbar/text-format/font.tsx
+++ client/src/components/RichTextContent/Toolbar/text-format/font.tsx
@@ -3,5 +3,27 @@ export const FontSelect = () => {
 };
 
 export const FontSizeInput = () => {
-  return null;
+  return (
+    <span>
+      <span className="relative">
+        <input className="w-10 h-6 border border-gray-300 text-center text-sm" />
+        <div className="font-size-dropdown">
+          <button>8</button>
+          <button>9</button>
+          <button>10</button>
+          <button>11</button>
+          <button>12</button>
+          <button>14</button>
+          <button>18</button>
+          <button>24</button>
+          <button>30</button>
+          <button>36</button>
+          <button>48</button>
+          <button>60</button>
+          <button>72</button>
+          <button>96</button>
+        </div>
+      </span>
+    </span>
+  );
 };
