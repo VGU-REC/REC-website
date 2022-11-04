e90061478e8a1bc198b68df9a55cbfbf5aaa2fb8
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-11-04 17:55:42 +0100

    fix controller

--- server/src/api/routes/routerController.ts
+++ server/src/api/routes/routerController.ts
@@ -1,5 +1,5 @@
-import { Router } from "express"
-import { CustomizeRouter } from "../../types"
+import { Router } from "express";
+import { CustomizeRouter } from "../../types";
 import {
   createController,
   deleteController,
@@ -7,22 +7,26 @@ import {
   updateController,
   countItemsController,
   getItemsController,
-} from "../controllers"
+} from "../controllers";
 
-import { EntityCollection } from "../entities"
-import { getbyID } from "../services/crud"
+import { EntityCollection } from "../entities";
+import { getbyID } from "../services/crud";
 
 export function routerController(
   router: Router,
   table: keyof typeof EntityCollection,
   option: Partial<CustomizeRouter> = { generateIdFrom: "title" }
 ) {
-  if (option.limit === undefined) option.limit = 30
-  router.get("/", (req, res) => getItemsController(req, res, table))
-  router.get("/items", (req, res) => countItemsController(req, res, table))
+  if (option.limit === undefined) option.limit = 30;
+  router.get("/", (req, res) => getItemsController(req, res, table));
+  router.get("/items", (req, res) => countItemsController(req, res, table));
   // cái này coi chừng nhầm lẫn với items có id là items :v
-  router.get("/:id", (req, res) => getController(req, res, getbyID, table))
-  router.post("/", (req, res) => createController(req, res, table, option as CustomizeRouter))
-  router.put("/:id", (req, res) => updateController(req, res, table, option as CustomizeRouter))
-  router.delete("/:id", (req, res) => deleteController(req, res, table))
+  router.get("/:id", (req, res) => getController(req, res, table));
+  router.post("/", (req, res) =>
+    createController(req, res, table, option as CustomizeRouter)
+  );
+  router.put("/:id", (req, res) =>
+    updateController(req, res, table, option as CustomizeRouter)
+  );
+  router.delete("/:id", (req, res) => deleteController(req, res, table));
 }
